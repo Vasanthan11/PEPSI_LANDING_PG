57500ae0764378f87f61cd58f16c2b6641fb6d5b
Vasanthan11 <vasavj70@gmail.com>
2023-09-28 14:40:57 +0530

    Pepsi Landing Page

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function imgSlider(newImg) {
+    const pepsi = document.querySelector('.pepsi').src = newImg;
+
+};
+
+function backColour(bgColor) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = bgColor;
+};
+
+function tColor(textColor) {
+    const specificLink = document.querySelector('.btn');
+    specificLink.style.color = textColor;
+};
